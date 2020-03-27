cd3a98a20aca9e815e46961a788ae7c5f6d3999c
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-27 13:22:05 +0000

    Allowed cross origin

--- index.js
+++ index.js
@@ -12,26 +12,34 @@ import indexRouter from './routes'
 const app = express()
 mongoose.set('useFindAndModify', false)
 
-app.use(logger('dev'))
-app.use(express.json())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://orthoproms.azurewebsites.net/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.use(logger('dev'));
+app.use(express.json());
 app.use(express.urlencoded({
     extended: false
-}))
+}));
 app.use(cookieParser());
 app.use(expressJwt({
     secret: config.jwtSecret
 }).unless({
     path: ['/', '/api', '/api/auth/signup', '/api/auth/signin', '/api/auth/code', '/api/auth/checkCode',
         '/api/auth/reset']
-}))
+}));
 
 app.get('/', (req, res) => {
     res.send('Welcome express')
 });
 
-app.use('/api', indexRouter)
+app.use('/api', indexRouter);
 
-mongoose.connect(`${config.db}`, { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(`${config.db}`, { useUnifiedTopology: true, useNewUrlParser: true });
 
 
 app.listen(6060, () => console.log('Running on localhost:6060'));
